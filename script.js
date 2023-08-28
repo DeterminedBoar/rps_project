e6f28d7be8906786b6fab3033064cf096b5f6199
Elijah Saenz <141266427+DeterminedBoar@users.noreply.github.com>
2023-08-28 14:37:18 -0700

    Add player choice function, modify playRound func
    Add player choice function to get players choice.
    Modify playRound function to work properly by
    changing the expression in the switch statement.

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let numberChoice = Math.floor(Math.random() * 3) + 1;
-    let choice
+    let choice;
     if (numberChoice === 1) {
-        choice = "Rock"
+        choice = "Rock";
     }
     else if (numberChoice === 2) {
-        choice = "Paper"
+        choice = "Paper";
     }
     else {
-        choice = "Scissors"
+        choice = "Scissors";
     }
-    return choice
+    return choice;
+};
+
+
+function getPlayerChoice() {
+    let playerChoice = prompt("What do you choose? Rock, Paper, Or Scissors?");
+    playerChoice = playerChoice.substring(0, 1).toUpperCase() + playerChoice.substring(1).toLowerCase();
+    
+    return playerChoice;
 };
 
 function playRound(computerSelection, playerSelection) {
     let result = ""
-    switch (computerSelection, playerSelection) {
-        case playerSelection === "Rock" && computerSelection === "Scissors":
-            result = "You Win! Rock beats Scissors!"
+    switch (playerSelection !== computerSelection) {
+        case (playerSelection == "Rock" && computerSelection == "Scissors"):
+            result = "You Win! Rock beats Scissors!";
+            playerScore += 1;
             break
-        case playerSelection === "Paper" && computerSelection === "Rock":
-            result = "You Win! Paper beats Rock!"
+        case (playerSelection == "Paper" && computerSelection == "Rock"):
+            result = "You Win! Paper beats Rock!";
+            playerScore += 1;
             break
-        case playerSelection === "Scissors" && computerSelection === "Paper":
-            result = "You Win! Scissors beats Paper!"
+        case (playerSelection == "Scissors" && computerSelection == "Paper"):
+            result = "You Win! Scissors beats Paper!";
+            playerScore += 1;
             break
-        case computerSelection === "Rock" && playerSelection === "Scissors":
-            result = "You Lose! Rock beats Scissors!"
+        case (computerSelection == "Rock" && playerSelection == "Scissors"):
+            result = "You Lose! Rock beats Scissors!";
+            computerScore += 1;
             break
-        case computerSelection === "Paper" && playerSelection === "Rock":
-            result = "You Lose! Paper beats Rock!"
+        case (computerSelection == "Paper" && playerSelection == "Rock"):
+            result = "You Lose! Paper beats Rock!";
+            computerScore += 1;
             break
-        case computerSelection === "Scissors" && playerSelection === "Paper":
-            result = "You Lose! Scissors beats Paper!"
-            break  
+        case (computerSelection == "Scissors" && playerSelection == "Paper"):
+            result = "You Lose! Scissors beats Paper!";
+            computerScore += 1;
+            break 
+        default:
+            result = "Invalid Input!"
     }
-    return result
+    return result;
 };
+
