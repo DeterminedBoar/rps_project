4dbb74b25136ce95d889a628ed8b7bc8fc02f6e5
Elijah Saenz <141266427+DeterminedBoar@users.noreply.github.com>
2023-08-28 13:26:47 -0700

    Add playRound function
    Add playRound function to allow user to play
    against the computer.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice() {
     let numberChoice = Math.floor(Math.random() * 3) + 1;
     let choice
@@ -15,3 +13,27 @@ function getComputerChoice() {
     return choice
 };
 
+function playRound(computerSelection, playerSelection) {
+    let result = ""
+    switch (computerSelection, playerSelection) {
+        case playerSelection === "Rock" && computerSelection === "Scissors":
+            result = "You Win! Rock beats Scissors!"
+            break
+        case playerSelection === "Paper" && computerSelection === "Rock":
+            result = "You Win! Paper beats Rock!"
+            break
+        case playerSelection === "Scissors" && computerSelection === "Paper":
+            result = "You Win! Scissors beats Paper!"
+            break
+        case computerSelection === "Rock" && playerSelection === "Scissors":
+            result = "You Lose! Rock beats Scissors!"
+            break
+        case computerSelection === "Paper" && playerSelection === "Rock":
+            result = "You Lose! Paper beats Rock!"
+            break
+        case computerSelection === "Scissors" && playerSelection === "Paper":
+            result = "You Lose! Scissors beats Paper!"
+            break  
+    }
+    return result
+};
